56d244a249503c584d1a7aa0e0c83eb85ba679ab
Rizal <rizal.firdaus117@gmail.com>
2023-05-21 18:26:42 +0700

    feat: splash screen final

--- lib/main.dart
+++ lib/main.dart
@@ -16,16 +16,49 @@ class SplashScreen extends StatelessWidget {
     return Scaffold(
         body: Container(
       width: double.infinity,
-      color: Colors.amber,
+      height: double.infinity,
+      decoration: const BoxDecoration(
+          image: DecorationImage(
+              fit: BoxFit.cover,
+              image: AssetImage('assets/images/background.png'))),
       child: Column(
         children: [
           Padding(
-            padding: const EdgeInsets.only(top: 100),
+            padding: const EdgeInsets.only(top: 50),
             child: Image.asset(
               'assets/logo/transparent-logo.png',
-              width: 300,
+              width: 400,
             ),
           ),
+          Stack(
+            alignment: AlignmentDirectional.center,
+            children: [
+              Image.asset(
+                'assets/images/miaw-group.png',
+                width: 400,
+                height: 453,
+                fit: BoxFit.cover,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xffF8A145),
+                  padding: const EdgeInsets.fromLTRB(30, 20, 30, 20),
+                  shadowColor: Colors.black,
+                  elevation: 20,
+                ),
+                onPressed: () {},
+                child: const Text(
+                  'MIAW ...',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 32,
+                    fontFamily: 'Gaegu',
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              )
+            ],
+          )
         ],
       ),
     ));
